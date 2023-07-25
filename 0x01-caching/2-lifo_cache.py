82d2b7ad56223b1f3485d735d400935a11e97e4f
H3c7o4 <hectorvladitok@gmail.com>
2023-07-25 15:45:39 +0200

    Create the LIFOCache class

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-''' FIFO caching: Create a class FIFOCache that inherits from BaseCaching
+''' LIFO Caching: Create a class LIFOCache that inherits from BaseCaching
                   and is a caching system
 '''
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    ''' A FIFO Cache.
+class LIFOCache(BaseCaching):
+    ''' A LIFO Cache.
         Inherits all behaviors from BaseCaching except, upon any attempt to
         add an entry to the cache when it is at max capacity (as specified by
-        BaseCaching.MAX_ITEMS), it discards the oldest entry to accommodate for
+        BaseCaching.MAX_ITEMS), it discards the newest entry to accommodate for
         the new one.
         Attributes:
           __init__ - method that initializes class instance
@@ -25,13 +25,15 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         ''' Add key/value pair to cache data.
             If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
-            discard oldest entry in cache to accommodate new entry. '''
+            discard newest entry in cache to accommodate new entry. '''
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.keys:
                 self.keys.append(key)
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
             if len(self.keys) > BaseCaching.MAX_ITEMS:
-                discard = self.keys.pop(0)
+                discard = self.keys.pop(-2)
                 del self.cache_data[discard]
                 print('DISCARD: {:s}'.format(discard))
 
