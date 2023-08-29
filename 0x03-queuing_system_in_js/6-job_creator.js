259c8ccd4d4baeda8a5c281b49a0b2ceaa2115f0
H3c7o4 <hectorvladitok@gmail.com>
2023-08-29 17:45:21 +0200

    Create a queue with kue and return the id

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,18 @@
+const kue = require('kue');
+
+const queue = kue.createQueue();
+const jobData = {
+  phoneNumber: '+237690280752',
+  message: 'Hello friends. My name is Hector',
+};
+const job = queue.create('push_notification_code', jobData).save((err) => {
+  if (!err) console.log(`Notification job created: ${job.id}`);  
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
