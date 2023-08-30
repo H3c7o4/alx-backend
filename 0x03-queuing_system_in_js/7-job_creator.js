1cb64f4982e727e78b9b4679d04c77ad9261eefd
H3c7o4 <hectorvladitok@gmail.com>
2023-08-30 15:49:26 +0200

    Create an array that will contain the blacklisted phone numbers

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -1,70 +1,69 @@
-const kue = require('kue');
+import kue from 'kue';
 
 const jobs = [
   {
     phoneNumber: '4153518780',
-    message: 'This is the code 1234 to verify your account'
+    message: 'This is the code 1234 to verify your account',
   },
   {
     phoneNumber: '4153518781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4153518743',
-    message: 'This is the code 4321 to verify your account'
+    message: 'This is the code 4321 to verify your account',
   },
   {
     phoneNumber: '4153538781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4153118782',
-    message: 'This is the code 4321 to verify your account'
+    message: 'This is the code 4321 to verify your account',
   },
   {
     phoneNumber: '4153718781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4159518782',
-    message: 'This is the code 4321 to verify your account'
+    message: 'This is the code 4321 to verify your account',
   },
   {
     phoneNumber: '4158718781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4153818782',
-    message: 'This is the code 4321 to verify your account'
+    message: 'This is the code 4321 to verify your account',
   },
   {
     phoneNumber: '4154318781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4151218782',
-    message: 'This is the code 4321 to verify your account'
-  }
+    message: 'This is the code 4321 to verify your account',
+  },
 ];
-const queue = kue.createQueue();
 
-for (const jobData in jobs) {
-  const job = queue.create('push_notification_code', jobData).save((err) => {
-  if (!err) console.log(`Notification job created: ${job.id}`);
-  else console.log(`Notification job ${job.id} failed: ${err}`);
+const queue = kue.createQueue();
+const queueKey = 'push_notification_code_2';
+
+jobs.forEach((jobObj) => {
+  const job = queue.create(queueKey, jobObj).save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
   });
 
   job.on('complete', () => {
     console.log(`Notification job ${job.id} completed`);
   });
 
-  let progress = 0;
-  const progressInterval = setInterval(() => {
-    progress = (progress + 10) > 100 ? 100: progress + 10;
-    console.log(`Notification job ${job.id} ${progress}% complete`);
+  job.on('failed', (errorMessage) => {
+    console.log(`Notification job ${job.id} failed: ${errorMessage}`);
+  });
 
-    if (progress === 100) {
-      clearInterval(progressInterval);
-    }
-  }, 1000);
-}
+  job.on('progress', (progress) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
+  });
+});
